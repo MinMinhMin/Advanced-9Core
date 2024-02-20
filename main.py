6bd2d54f265156b1eef7db41b888e40b6de0dd9e
MinMinhMin <xyzhuy2@gmail.com>
2024-02-20 08:53:31 +0700

    GUI

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ import data.controlaudio
 import os
 import data.Reverb
 import data.Pitchshifter
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
 
 
 def resource_path(relative):
@@ -223,17 +226,18 @@ class ListFrame(ttk.Frame):
 
 
 class PlayButton(ctk.CTkButton):
-    def __init__(self, parent):
+    def __init__(self, parent, win_width, win_height):
         self.Image1 = ctk.CTkImage(
             light_image=Image.open(resource_path('play_button1.png')),
             dark_image=Image.open(resource_path('play_button1.png')),
-            size=(238, 238)
+            size=(int(win_width * 0.2 * 0.75), int(win_height * 0.426 * 0.75))
+
         )
 
         self.Image2 = ctk.CTkImage(
             light_image=Image.open(resource_path('play_button2.png')),
             dark_image=Image.open(resource_path('play_button2.png')),
-            size=(238, 238)
+            size=(int(win_width * 0.2 * 0.75), int(win_height * 0.426 * 0.75))
         )
 
         super().__init__(parent, text=' ', fg_color='white', image=self.Image1, hover_color='white')
@@ -288,14 +292,14 @@ class AddOptionMod(ctk.CTkFrame):
         self.label = ctk.CTkLabel(self, text=text, text_color='black')
         self.label.grid(row=0, column=0)
 
-        self.option_var = ctk.StringVar(value='Default')
+        self.option_var = ctk.StringVar(value='None')
         self.option = ctk.CTkOptionMenu(self, values=['Default', f'{type1}', f'{type2}', f'{type3}', f'{type4}'],
                                         variable=self.option_var)
         self.option.grid(row=0, column=1, columnspan=2)
 
 
 class timeframe(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, win_width, win_height):
         super().__init__(parent, fg_color='white')
 
         self.start_time = None
@@ -313,28 +317,33 @@ class timeframe(ctk.CTkFrame):
 
         self.time_slider_var = ctk.DoubleVar(value=1)
 
-        self.time_slider = ctk.CTkSlider(self, width=300, height=20,
+        self.time_slider_width = int(win_width * 0.2343)
+        self.time_slider_height = int(win_height * 0.03)
+
+        self.time_slider = ctk.CTkSlider(self,
                                          button_color='#f5bcf5', button_hover_color='#f294f2',
                                          variable=self.time_slider_var)
 
         self.label_time.grid(row=0, column=0)
 
-        self.time_slider.grid(row=0, column=1, columnspan=3)
+        self.time_slider.grid(row=0, column=1, columnspan=3, sticky='ew')
 
-        self.place(relx=0.49, rely=0.8, anchor='center', relwidth=0.3, relheight=0.2)
+        self.place(relx=0.5, rely=0.8, anchor='center', relwidth=0.3, relheight=0.2)
 
         self.small_label = ctk.CTkLabel(self)
 
 
 class App(ttk.Window):
 
-    def __init__(self, title, size):
+    def __init__(self, title):
         super().__init__()
 
         # window setting
         self.resizable(False, False)
         self.title(title)
-        self.geometry(f'{size[0]}x{size[1]}')
+        self.width = int((self.winfo_screenwidth() * 4) / 5)
+        self.height = int((self.winfo_screenheight() * 2) / 3)
+        self.geometry(f'{self.width}x{self.height}')
 
         # logo
         self.iconbitmap('icon.ico')
@@ -366,7 +375,7 @@ class App(ttk.Window):
         self.add_path_from_log()
 
         # playbutton
-        self.playbutton = PlayButton(self)
+        self.playbutton = PlayButton(self, self.width, self.height)
         self.playbutton.configure(command=self.play_current_song)
         self.playbutton.alert_1 = ToastNotification(
             title='ERROR',
@@ -424,14 +433,16 @@ class App(ttk.Window):
         )
 
         # time bar
-        self.timeframe = timeframe(self)
+        self.timeframe = timeframe(self, self.width, self.height)
         self.timeframe.place_forget()
+        self.timeframe.tkraise(aboveThis=self.playbutton)
         self.timeframe.time_slider.bind('<Motion>', lambda event: self.func1(event))
         self.timeframe.time_slider.bind('<Leave>', lambda event: self.func2(event))
         self.timeframe.time_slider.bind('<B1-Motion>', lambda event: self.func3(event))
         self.timeframe.time_slider.bind('<ButtonRelease-1>', lambda event: self.func4(event))
         self.hold_mouse = False
         self.time = 0
+        self.end_song = False
         self.AFTER = None
         self.mainloop()
 
@@ -442,12 +453,13 @@ class App(ttk.Window):
             self.browse_func2()
 
     def func1(self, event):
-        self.timeframe.time = int((event.x / 300) * self.timeframe.end_time)
+        self.timeframe.time = min(max(int((event.x / self.timeframe.time_slider_width) * self.timeframe.end_time), 0),
+                                  self.timeframe.end_time)
         self.timeframe.small_label.place_forget()
-        self.timeframe.small_label.place(x=event.x + 20, y=event.y - 10, anchor='nw')
+        self.timeframe.small_label.place(x=event.x * 1.03, y=event.y, anchor='center')
         self.timeframe.small_label.configure(
             text=f'{self.change_time(self.timeframe.time)}/{self.change_time(self.timeframe.end_time)}',
-            text_color='black')
+            text_color='black', font=('Calibri', 11))
         self.timeframe.time_slider.tkraise(aboveThis=self.timeframe.small_label)
 
     def func2(self, event):
@@ -455,13 +467,15 @@ class App(ttk.Window):
         self.hold_mouse = False
 
     def func3(self, event):
-        self.timeframe.time = min(max(int((event.x / 300) * self.timeframe.end_time), 0), self.timeframe.end_time)
+        self.timeframe.time = min(max(int((event.x / self.timeframe.time_slider_width) * self.timeframe.end_time), 0),
+                                  self.timeframe.end_time)
         self.timeframe.small_label.place_forget()
-        self.timeframe.small_label.place(x=event.x + 20, y=event.y - 10, anchor='nw')
+        self.timeframe.small_label.place(x=event.x * 1.03, y=event.y, anchor='center')
         self.timeframe.small_label.configure(
             text=f'{self.change_time(self.timeframe.time)}/{self.change_time(self.timeframe.end_time)}',
-            text_color='black')
+            text_color='black', font=('Calibri', 11))
         self.timeframe.time_slider.tkraise(aboveThis=self.timeframe.small_label)
+
         pygame.mixer.music.pause()
         self.hold_mouse = True
         self.set_pause_button()
@@ -472,6 +486,9 @@ class App(ttk.Window):
         self.time = self.timeframe.time
         pygame.mixer.music.unpause()
         self.hold_mouse = False
+        if self.end_song == True:
+            self.start_time()
+            self.end_song = False
         self.set_play_button()
 
     def change_time(self, time):
@@ -494,8 +511,9 @@ class App(ttk.Window):
         if self.hold_mouse == True or self.list.song[self.list.current_song_index].play_click % 2 == 0:
             pass
             self.AFTER = self.after(1000, self.start_time)
-        if int(self.timeframe.time_slider_var.get()) > self.timeframe.end_time:
+        if int(self.timeframe.time_slider_var.get()) >= self.timeframe.end_time:
             pygame.mixer.music.pause()
+            self.end_song = True
             self.after_cancel(self.AFTER)
 
     def audioprocess(self):
@@ -559,6 +577,9 @@ class App(ttk.Window):
     def browse_func1(self):
 
         self.directory_button.file_name = filedialog.askdirectory()
+        self.enable_play_button()
+        self.enable_apply_button()
+        self.end_song = False
         self.reset()
 
         with open('library_path.txt', 'w') as file:
@@ -579,6 +600,7 @@ class App(ttk.Window):
 
         self.directory_button.file_name = self.content
         self.directory_button.titlesong.place_forget()
+        self.end_song = False
         self.directory_button.titlesong.place(relx=0, rely=0.1, anchor='nw')
 
         mfdir = self.directory_button.file_name
@@ -653,29 +675,29 @@ class App(ttk.Window):
         self.playbutton.Image1.configure(
             light_image=Image.open(resource_path('play_button1.png')),
             dark_image=Image.open(resource_path('play_button1.png')),
-            size=(238, 238))
+            size=(int(self.width * 0.2 * 0.75), int(self.height * 0.426 * 0.75)))
 
         self.playbutton.Image2.configure(
             light_image=Image.open(resource_path('play_button2.png')),
             dark_image=Image.open(resource_path('play_button2.png')),
-            size=(238, 238))
+            size=(int(self.width * 0.2 * 0.75), int(self.height * 0.426 * 0.75)))
 
     def set_play_button(self):
         self.playbutton.Image1.configure(
             light_image=Image.open(resource_path('pause_button1.png')),
             dark_image=Image.open(resource_path('pause_button1.png')),
-            size=(238, 238))
+            size=(int(self.width * 0.2 * 0.75), int(self.height * 0.426 * 0.75)))
 
         self.playbutton.Image2.configure(
             light_image=Image.open(resource_path('pause_button2.png')),
             dark_image=Image.open(resource_path('pause_button2.png')),
-            size=(238, 238))
+            size=(int(self.width * 0.2 * 0.75), int(self.height * 0.426 * 0.75)))
 
     def reset(self):
         self.set_pause_button()
         data.controlaudio.stop_song()
         self.timeframe.destroy()
-        self.timeframe = timeframe(self)
+        self.timeframe = timeframe(self, self.width, self.height)
         self.timeframe.place_forget()
         self.timeframe.time_slider.bind('<Motion>', lambda event: self.func1(event))
         self.timeframe.time_slider.bind('<Leave>', lambda event: self.func2(event))
@@ -691,6 +713,8 @@ class App(ttk.Window):
             self.list.song[index].configure(fg_color='#f5bcf5')
             self.list.previous_song_index = self.list.current_song_index
             self.list.current_song_index = index
+            self.enable_apply_button()
+            self.enable_play_button()
 
             if (self.list.previous_song_index != None) and (
                     self.list.previous_song_index != self.list.current_song_index):
@@ -700,13 +724,16 @@ class App(ttk.Window):
                 self.list.song[self.list.previous_song_index].apply_mode_button_count = 0
                 self.Mode1.slider.set(self.Mode1.start_value)
                 self.Mode2.slider.set(self.Mode2.start_value)
-                self.Mode3.option.set('Default')
+                self.Mode3.option.set('None')
+                self.end_song = False
+
             else:
                 self.list.song[self.list.current_song_index].is_choose = True
                 self.list.song[self.list.current_song_index].play_click = 0
+                self.end_song = False
 
         return song_func_in
 
 
 # run
-App('Advanced 9Core', (1280, 600))
+App('Advanced 9Core')
